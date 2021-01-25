c571e12d9a6277e4dc483aafacb78f4453b16382
Kaiden-exe <kaiden.sewradj@gmail.com>
2021-01-25 16:26:22 +0100

    new hill climber

--- code/algorithms/hill_climber.py
+++ code/algorithms/hill_climber.py
@@ -1,12 +1,14 @@
 from .random import Random
 from code.classes.protein import Protein, Temp_Protein
-from random import choice, getrandbits
+from random import choice, getrandbits, randint
+from depth_first import DepthFirst
 import copy
 
 class HillClimber(Random):
     def __init__(self, protein):
         super().__init__()
         self.best = self.get_start(protein)
+
 
     def get_start(self, protein):
         '''
@@ -15,6 +17,7 @@ class HillClimber(Random):
         self.run_random(protein, 1)
         return protein
     
+
     def hike(self, iterations, mutations):
         '''
         Runs the hill climber algorithm.
@@ -40,6 +43,7 @@ class HillClimber(Random):
             else:
                 del new 
     
+
     def mutate(self, protein):
         '''
         Changes the coordinates of a single aminoacid.
@@ -145,6 +149,7 @@ class HillClimber(Random):
                 break
         aminoacid.folding = folding
 
+
     def add_solution(self, protein):
         '''
         Add a solution to the list of solutions.
@@ -153,6 +158,7 @@ class HillClimber(Random):
         copy_dict = copy.deepcopy(protein.positions)
         self.solutions.append([score, copy_dict])
 
+
     def get_best(self):
         '''
         Returns a list of score and a dictionary of the best found folding.
@@ -160,15 +166,15 @@ class HillClimber(Random):
         return [self.best.score, self.best.positions]
 
 
-class HillClimber_Alt(HillClimber):
-    def hike(self, iterations, mutations):
+class HillClimber_Cut(HillClimber):
+    def hike(self, iterations):
         '''
         Runs the alternative hill climber. 
         '''
         for i in range(iterations):
             new = copy.deepcopy(self.best)
 
-            self.mutate(new, mutations)
+            self.mutate(new)
 
             new.set_stability()
             self.add_solution(new)
@@ -185,19 +191,123 @@ class HillClimber_Alt(HillClimber):
             else:
                 del new 
 
-    def mutate(self, protein, mutations):
+    def mutate(self, protein):
         '''
         Makes a single mutation of mutations length. 
         '''
         
         # Get aminoacids to rearrange
-        temp_protein = self.get_aminoacids(protein, mutations)
+        while True:
+            temp_protein = self.get_aminoacids(protein)
+            end_cor = self.get_end_coordinates(protein, temp_protein)
+            depth_first = DepthFirst_Climber(temp_protein)
+            depth_first.run()
+            new_foldings = self.pick_solution(depth_first, end_cor)
 
+            if len(new_foldings) > 0:
+                break
+            else:
+                # TODO: Delete everything, before doing it all again
+                # TODO: save the failed length etc.
+                pass
+            
     
-    def get_aminoacids(self, protein, mutations):
-
+    def get_aminoacids(self, protein):
+        '''
+        Returns a sub-string of aminoacids from the protein a temporary protein
+        '''
+        
+        # Get a random chain length 
         protein_length = len(protein.aminoacids)
-        start_choices = [i for i in range(protein_length - mutations + 1)]
-        start = choice(start_choices)
-        aminoacids = protein.aminoacids[start:start + mutations]
-        temp_protein = Temp_Protein()
+        min_length = 3
+        max_length = int(protein_length / 4)
+        if max_length < min_length:
+            max_length = min_length
+        chain_length = randin(min_length, max_length)
+
+
+        last_option = protein_length - chain_length
+        start = randint(0, last_option)
+        aminoacids = protein.aminoacids[start:start + chain_length]
+        
+        return Temp_Protein(aminoacids)
+        
+    def get_end_coordinates(self, protein, temp_protein):
+        '''
+        Calculates and returns the coordinates the last aminoacid needs when the start is at (0,0).
+        '''
+
+        sorted_acids = protein.get_sorted_positions()
+        start_index = temp_protein.aminoacids[0].index
+        end_index = temp_protein.aminoacids[-1].index
+        start_cor = sorted_acids[start_index][0]
+        end_cor = sorted_acids[end_index][0]
+
+        delta_x = end_cor[0] - start_cor[0]
+        delta_y = end_cor[1] - start_cor[1]
+
+        return (delta_x, delta_y)
+
+
+    def pick_solution(self, depth_first, end_cor):
+        '''
+        Checks all solutions for a valid folding
+        '''
+        solutions = depth_first.solutions
+        solutions.sort(key=lambda x:x[0], reverse=True)
+
+        for solution in solutions:
+            positions = solution[1].items()
+            positions.sort(key=lambda x:x[1].index)
+            
+            if positions[-1][0] == end_cor:
+                # TODO: check if acids are directly edited. If yes, you only have to change the coordinates
+                return solutions[1].values()
+        
+        return []
+
+
+class HillClimber_Pull(HillClimber):
+    def __init__(protein):
+        super().__init__(protein)
+        self.success = 0
+
+    def mutate(self, protein):
+
+        # Pick random acid, but not the last one
+        i_plus = choice([i for i in protein.positions.items() if i[1] != protein.aminoacids[-1]])
+        i = protein.aminoacids[i_plus[1].index - 1]
+        sorted_acids = protein.get_sorted_positions()
+        i_cor = sorted_acids[i.index][0]
+
+        # Get free coordinates of i-1 and coordinates of i
+        surrounding_coordinates = protein.get_surrounding_coordinates(i_plus[0][0], i_plus[0][1])
+        free_coordinates = self.get_free_coordinates(protein, surrounding_coordinates)
+
+        # See if you have free coordinates for i
+        if len(free_coordinates) > 0:
+            
+            for L in free_coordinates:
+                if i.folding in [-1, 1]:
+                    Cx = L[0] + i_min_fold
+                    Cy = L[1]
+                else:
+                    Cx = L[0]
+                    Cy = L[1] + int(i_min_fold/2)
+                
+                C = (Cx, Cy)
+
+                if not C in protein.positions.keys() and C in protein.get_surrounding_coordinates(i_cor[0], i_cor[1]):
+                    
+                    self.pull(protein, i, L, C)
+                    self.success += 1
+                    break
+
+
+    def pull(self, protein, i, L, C):
+        
+        acid = i
+        minus = protein.aminoacids[i.index - 1]
+
+
+
